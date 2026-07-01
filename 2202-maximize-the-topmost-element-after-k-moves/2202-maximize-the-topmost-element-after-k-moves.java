1fe9ccf9f9b7f70006926eab4e671c48d5a6f893
Sandeep Kumar <158429072+Sandeep0999@users.noreply.github.com>
2026-07-01 20:39:20 +0530

    Time: 1 ms (97.06%), Space: 78.7 MB (45.88%) - LeetHub

---
+++ 2202-maximize-the-topmost-element-after-k-moves/2202-maximize-the-topmost-element-after-k-moves.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumTop(int[] nums, int k) {
+        int n = nums.length;
+        if (k == 0) return nums[0];
+        if (n == 1) return (k % 2 == 0) ? nums[0] : -1;
+
+        int max = Integer.MIN_VALUE;
+        // if k == 1 first element need to be pop up 
+        for(int i=0; i < Math.min(k-1, n); i++){
+            max = Math.max(max, nums[i]);
+        }
+        if(k < n) max = Math.max(max, nums[k]);
+        return max;
+    }
+}
