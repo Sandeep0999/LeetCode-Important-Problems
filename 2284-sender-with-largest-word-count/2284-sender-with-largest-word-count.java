e99066e60d9b61f0731ba38263359bf34da7aae5
Sandeep Kumar <158429072+Sandeep0999@users.noreply.github.com>
2026-07-08 20:12:35 +0530

    Time: 53 ms (51.88%), Space: 58 MB (43.99%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -1,22 +1,41 @@
 class Solution {
-    public String largestWordCount(String[] m, String[] s) {
-        int n = m.length;
-        TreeMap<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
-        for(int i=0; i<n; i++){
-            String arr[] = m[i].split(" ");
-            int len = arr.length;
-            map.put(s[i], map.getOrDefault(s[i], 0) + len);
+    public String largestWordCount(String[] messages, String[] senders) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < messages.length; i++) {
+            map.put(senders[i],
+                    map.getOrDefault(senders[i], 0) + messages[i].split(" ").length);
         }
-        String ans = " ";
-        int max = 0;
-        for(Map.Entry<String, Integer> en : map.entrySet()){
-            int freq = en.getValue();
-            String name = en.getKey();
-            if(freq > max){
-                ans = name;
-                max = freq;
+
+        String ans = "";
+        int max = -1;
+
+        for (String sender : map.keySet()) {
+            int cnt = map.get(sender);
+            if (cnt > max || (cnt == max && sender.compareTo(ans) > 0)) {
+                max = cnt;
+                ans = sender;
             }
         }
+
         return ans;
+        // int n = m.length;
+        // TreeMap<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+        // for(int i=0; i<n; i++){
+        //     String arr[] = m[i].split(" ");
+        //     int len = arr.length;
+        //     map.put(s[i], map.getOrDefault(s[i], 0) + len);
+        // }
+        // String ans = " ";
+        // int max = 0;
+        // for(Map.Entry<String, Integer> en : map.entrySet()){
+        //     int freq = en.getValue();
+        //     String name = en.getKey();
+        //     if(freq > max){
+        //         ans = name;
+        //         max = freq;
+        //     }
+        // }
+        // return ans;
     }
 }
