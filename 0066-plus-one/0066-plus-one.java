c0c79bc49221c90e20599d8c4d460a9c56906732
Sandeep Kumar <158429072+Sandeep0999@users.noreply.github.com>
2026-07-11 16:44:14 +0530

    Time: 0 ms (100%), Space: 43.2 MB (90.44%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] d) {
+        int n = d.length;
+
+        for(int i = n-1; i >= 0; i--){
+            if(d[i] != 9){
+                d[i] += 1;
+                return d;
+            }
+            d[i] = 0;
+        }
+        int ans[] = new int[n+1];
+        ans[0] = 1;
+        return ans;
+    }
+}
